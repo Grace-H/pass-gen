97d9860d5967f97968bfecabc747a3c8ea1c534e
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-03-11 09:52:09 -0800

    generate alpha syllables

--- pass-gen.c
+++ pass-gen.c
@@ -6,16 +6,37 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <openssl/rand.h>
 
-#define RAND_BYTES 8
+#define RAND_BYTES 4
+#define MAX_BEGS 46
+#define MAX_MIDS 12
+#define MAX_ENDS 45
 
-int generator(char syll[4]);
-int syllable(char syll[4]);
+char begs[MAX_BEGS][3] = {
+  "b\0", "c\0","d\0", "f\0", "g\0", "h\0", "j\0", "k\0", "l\0", "m\0",
+  "n\0", "p\0", "q\0", "r\0", "s\0", "t\0", "v\0", "w\0", "x\0", "y\0",
+  "z\0","sh\0", "bl\0", "fl\0", "gl\0", "pl\0", "sl\0", "br\0", "cr\0", "dr\0",
+  "fr\0", "gr\0", "pr\0", "tr\0", "sc\0", "sk\0", "sm\0", "sn\0", "sw\0", "tw\0",
+  "ch\0", "th\0", "cl\0", "sp\0", "st\0", "dw\0"  };
+char mids[MAX_MIDS][3] = {
+  "a\0", "e\0", "i\0", "o\0", "u\0", "ai\0", "ee\0", "oo\0", "ow\0", "ou\0",
+  "ea\0", "ie\0"  };
+char ends[MAX_ENDS][3] = {
+    "b\0", "c\0", "d\0", "f\0", "g\0", "h\0", "j\0", "k\0", "l\0", "m\0",
+    "n\0", "p\0", "r\0", "s\0", "t\0", "v\0", "w\0", "x\0", "z\0", "sh\0",
+    "rb\0", "rd\0", "rd\0", "sk\0", "sh\0", "ch\0", "th\0", "rm\0", "en\0", "st\0",
+    "lk\0", "ll\0", "ck\0", "ft\0", "lt\0", "nt\0", "xt\0", "lb\0", "ld\0", "lp\0",
+    "nk\0", "sp\0", "mp\0", "zz\0", "ff\0" };
+
+int generator(char *syll, int n);
+int syllable(char *syll, int n);
 
 int main(int argc, char **argv){
   int l = 0, s = 2, n = 2, m = 8;
   int c = 0; // copy to clipboard
+  char pass[16];
   
   int opt;
   while((opt = getopt(argc, argv, ":l:s:n:ch")) != -1){
@@ -43,12 +64,14 @@ int main(int argc, char **argv){
       break;
     }
   }
-  
-  printf("password123\n");
+
+  generator(pass, 16);
+  printf("%s\n", pass);
   return 0;
 }
 
-int generator(char syll[4]){
+// returns length of generated segment, negative on failure
+int generator(char *syll, int n) {
   int len = 0;
   unsigned char buf[RAND_BYTES];
   
@@ -72,12 +95,50 @@ int generator(char syll[4]){
   return len;
 }
 
-int syllable(char syll[4]){
+// generate alpha syllable
+// params: syll (recommended min length 7)
+// returns length of syllable, negative on failure
+int syllable(char *syll, int n){
   int len = 0;
   unsigned char buf[RAND_BYTES];
 
+  // beginning
   int ret = RAND_bytes(buf, RAND_BYTES);
   if (ret < 1)
     return -1;
+
+  int begi = (int) *buf % MAX_BEGS;
+  int begz = strlen(begs[begi]);
+
+  // middle
+  ret = RAND_bytes(buf, RAND_BYTES);
+  if (ret < 1)
+    return -1;
+
+  int midi = (int) *buf % MAX_MIDS;
+  int midz = strlen(mids[midi]);
+
+  // end
+  ret = RAND_bytes(buf, RAND_BYTES);
+  if (ret < 1)
+    return -1;
+
+  int endi = (int) *buf % MAX_ENDS;
+  int endz = strlen(ends[endi]);
+
+  // assemble
+  if (len + begz + midz + endz < n) {
+    strncpy(&syll[len], begs[begi], begz);
+    len += begz;
+    strncpy(&syll[len], mids[midi], midz);
+    len += midz;
+    strncpy(&syll[len], ends[endi], endz);
+    len += endz;
+  } else {
+    fprintf(stderr, "segment too short\n");
+    return -1;
+  }
+  syll[len] = '\0';
+
   return len;
 }
